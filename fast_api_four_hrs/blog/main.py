f6ddf109b0ff5e4f19840aead6411c14381a0196
Aliaksandr Suzen <asuzen@allot.com>
2022-10-04 16:35:34 +0300

    First commit Includes some routes to get blogs and the one to post the new one

---
+++ fast_api_four_hrs/blog/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from . import models
+from fast_api_four_hrs.blog import schemas
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/blog")
+def all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{blog_id}", status_code=200)
+def show(blog_id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id {blog_id} is not available")
+    return blog
