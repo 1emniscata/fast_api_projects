527b542228c4a4a68beefc521ff194670a1068e5
Aliaksandr Suzen <asuzen@allot.com>
2022-10-05 17:16:35 +0300

    Adds authorization

--- fast_api_four_hrs/blog/routers/authentication.py
+++ fast_api_four_hrs/blog/routers/authentication.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from fast_api_four_hrs.blog import schemas, models
@@ -12,7 +13,8 @@ router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(),
+          db: Session = Depends(get_db)):
     # user = db.query(models.User).filter(models.User.email == request.username).first()
     user = db.query(models.User).where(models.User.email == request.username).first()
     if not user:
@@ -23,6 +25,7 @@ def login(request: schemas.Login, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Incorrect password")
     # return user
+
     # return "login"
     # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
--- fast_api_four_hrs/blog/routers/blog.py
+++ fast_api_four_hrs/blog/routers/blog.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from fast_api_four_hrs.blog import schemas, models
 from fast_api_four_hrs.blog.database import get_db
+from fast_api_four_hrs.blog.oauth2 import get_current_user
 from fast_api_four_hrs.blog.repository import blog
 
 router = APIRouter(
@@ -14,24 +15,28 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.ShowBlog])
-def all_blogs(db: Session = Depends(get_db)):
+def all_blogs(db: Session = Depends(get_db),
+              current_user: schemas.User = Depends(get_current_user)):
     # blogs = db.query(models.Blog).all()
     # return blogs
     return blog.get_all(db)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, db: Session = Depends(get_db),
+           current_user: schemas.User = Depends(get_current_user)):
     # new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     # db.add(new_blog)
     # db.commit()
     # db.refresh(new_blog)
     # return new_blog
-    return blog.create(request , db)
+    return blog.create(request, db)
 
 
-@router.delete("/{blog_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete(blog_id, db: Session = Depends(get_db)):
+@router.delete("/{blog_id}", status_code=status.HTTP_204_NO_CONTENT,
+               )
+def delete(blog_id, db: Session = Depends(get_db),
+           current_user: schemas.User = Depends(get_current_user)):
     # blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
     # if not blog.first():
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -44,7 +49,8 @@ def delete(blog_id, db: Session = Depends(get_db)):
 
 
 @router.put("/{blog_id}", status_code=status.HTTP_202_ACCEPTED)
-def update(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(blog_id, request: schemas.Blog, db: Session = Depends(get_db),
+           current_user: schemas.User = Depends(get_current_user)):
     # blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
     # if not blog.first():
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -57,7 +63,8 @@ def update(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 @router.get("/{blog_id}", status_code=200, response_model=schemas.ShowBlog)
-def show(blog_id, db: Session = Depends(get_db)):
+def show(blog_id, db: Session = Depends(get_db),
+         current_user: schemas.User = Depends(get_current_user)):
     # blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     # if not blog:
     #     # response.status_code = status.HTTP_404_NOT_FOUND
